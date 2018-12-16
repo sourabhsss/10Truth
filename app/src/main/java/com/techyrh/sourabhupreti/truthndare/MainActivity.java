5806f8e2dac39d57101006ddc51ec362755c0087
Sourabh Upreti <sourabhupreti16@gmail.com>
2018-12-16 14:20:28 +0530

    Initial commit

---
+++ app/src/main/java/com/techyrh/sourabhupreti/truthndare/MainActivity.java
@@ -0,0 +1,13 @@
+package com.techyrh.sourabhupreti.truthndare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
